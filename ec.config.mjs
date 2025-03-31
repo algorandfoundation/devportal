817beeb208fc821c8c665f5c8a28f7859730547f
Michael Feher <github@phearzero.com>
2025-03-31 12:02:58 -0400

    fix(expressive): patch teal language

--- ec.config.mjs
+++ ec.config.mjs
@@ -1,12 +1,17 @@
 import mocha from '@catppuccin/vscode/themes/mocha.json' with { type: 'json' };
 import latte from '@catppuccin/vscode/themes/latte.json' with { type: 'json' };
-import fs from 'node:fs';
+import teal from './src/utils/teal-syntax.json' with { type: 'json' };
+
 
 /** @type {import('@astrojs/starlight/expressive-code').StarlightExpressiveCodeOptions} */
 export default {
   themes: [latte, mocha],
   shiki: {
-    langs: [JSON.parse(fs.readFileSync('src/utils/teal-syntax.json', 'utf-8'))],
+    bundledLangs: ['python', 'typescript', 'teal', 'abnf'],
+    langs: [{
+      ...teal,
+      name: 'teal',
+    }],
   },
   styleOverrides: {
     borderRadius: '0.5rem',
