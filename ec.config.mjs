22e10528feeca149b75b1c1addd9bb9e5d953b7b
Michael Feher <github@phearzero.com>
2025-03-31 12:21:30 -0400

    build(expressive): support casing for LLM, remove warnings

--- ec.config.mjs
+++ ec.config.mjs
@@ -3,12 +3,29 @@ import latte from '@catppuccin/vscode/themes/latte.json' with { type: 'json' };
 import teal from './src/utils/teal-syntax.json' with { type: 'json' };
 
 
+const stub = {
+  "$schema": "https://raw.githubusercontent.com/martinring/tmlanguage/master/tmlanguage.json",
+  "patterns": [],
+  "scopeName": "source.txt"
+}
+
+
 /** @type {import('@astrojs/starlight/expressive-code').StarlightExpressiveCodeOptions} */
 export default {
   themes: [latte, mocha],
   shiki: {
-    bundledLangs: ['python', 'typescript', 'teal', 'abnf'],
-    langs: [{
+    bundledLangs: ['python', 'typescript', 'teal', 'abnf', 'TEAL'],
+    langs: [
+      {
+       ...stub,
+       name: 'none'
+      },
+      {
+        ...stub,
+        name: 'default'
+      },
+      teal,
+      {
       ...teal,
       name: 'teal',
     }],
